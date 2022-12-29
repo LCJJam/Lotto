932e597268f5e8bf933dc0b2d288f77c7759e00b
LC_home <dlckd125@nate.com>
2022-12-30 01:54:12 +0900

    Test : Spring Boot Study Day 5 - OAuth2 , Naver test Spring Security and TestCode

--- Lotto_springboot_dev/src/test/java/com/lotto/dev/web/HelloControllerTest.java
+++ Lotto_springboot_dev/src/test/java/com/lotto/dev/web/HelloControllerTest.java
@@ -1,9 +1,16 @@
 package com.lotto.dev.web;
 
+import com.lotto.dev.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,12 +19,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class) // Junit4에 있는 스프링 실행자
-@WebMvcTest(controllers = HelloController.class) // SpringMvc 에 집중할 수 있는 어노테이션
+@WebMvcTest(controllers = HelloController.class,
+                excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                classes = SecurityConfig.class)
+                }) // SpringMvc 에 집중할 수 있는 어노테이션
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc; // 웹 API를 테스트 할 때 사용
 
     @Test
+    @WithMockUser (roles = "USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -27,6 +39,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser (roles = "USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
--- Lotto_springboot_dev/src/test/java/com/lotto/dev/web/domain/posts/PostsRepositoryTest.java
+++ Lotto_springboot_dev/src/test/java/com/lotto/dev/web/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
