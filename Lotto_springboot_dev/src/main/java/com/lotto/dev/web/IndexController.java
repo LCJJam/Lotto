3d7992b22fbd2afedb55ec9e22f7e7984db3f4f4
LC_home <dlckd125@nate.com>
2023-01-02 03:57:07 +0900

    Test : Spring Boot Study Day 6 - 경로 재설정

--- Lotto_springboot_dev/src/main/java/com/lotto/dev/web/IndexController.java
+++ Lotto_springboot_dev/src/main/java/com/lotto/dev/web/IndexController.java
@@ -19,7 +19,7 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
 
-    @GetMapping("/")
+    @GetMapping({"","/"})
     public String index(Model model) {
         model.addAttribute("posts",postsService.findAllDesc());
 
