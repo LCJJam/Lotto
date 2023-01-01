83e669d3a6ca28dfd74b4fb80c82f01a4bc6ca71
LC_home <dlckd125@nate.com>
2023-01-02 02:55:37 +0900

    Test : Spring Boot Study Day 6 - 경로 재설정

--- Lotto_springboot_dev/src/main/java/com/lotto/dev/config/WebConfig.java
+++ Lotto_springboot_dev/src/main/java/com/lotto/dev/config/WebConfig.java
@@ -1,17 +1,29 @@
 package com.lotto.dev.config;
 
+import com.lotto.dev.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/js/**")
                 .addResourceLocations("classpath:/js/")
                 .setCachePeriod(20)
         ;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
     }
 }
