ec2ccbd8edbc148f253de208d0e312ee8909af57
LC_MAC <dlckd125@nate.com>
2023-08-24 23:29:40 +0900

    chore : react 로그인 화면 typeScript 적용 - js -> tsx 로 변환 및 import 전환 진행

---
+++ Lotto_react_dev/my-app/src/index.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import { AuthContextProvider } from './store/auth-context';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <AuthContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthContextProvider>,
+);
